cd5366a97ac22eafd2da27f0fe02759d72a8ef4d
sbibik <sbibik@wavesplatform.com>
2019-12-02 13:25:44 +0300

    step

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,7 @@
 const processEnv = process.env;
 const envIsDev = processEnv.isDev;
 const corePath = processEnv.corePath || 'core.docs.wavesplatform';
+const envHost = processEnv.host || '127.0.0.1';
 if(envIsDev) {
     const inspector = require('inspector');
     inspector.open(9229, '127.0.0.1');
@@ -13,9 +14,23 @@ const getVuepressConfig = coreDocsWavesplatform.getVuepressConfig;
 const enLocaleConfig = require('./locales/en');
 const ruLocaleConfig = require('./locales/ru');
 const colorationConfig = require('./colouration');
+
 module.exports = (ctx) => {
     const vuepressComputedConfig = getVuepressConfig(ctx, {
+        host: envHost,
         dest: path.join(rootDir, 'dist'),
+        locales: {
+            '/en/': {
+                lang: 'en-US',
+                title: 'Wavesplatform Docs',
+                description: 'Waves documentation in English'
+            },
+            '/ru/': {
+                lang: 'ru-RU',
+                title: 'Wavesplatform Документация',
+                description: 'Документация Waves на русском',
+            }
+        },
         themeConfig: {
             // activeColouration: 'default',
             colouration: colorationConfig,
