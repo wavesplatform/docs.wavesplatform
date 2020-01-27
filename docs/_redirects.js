d4f3ce5924ad8cd6748cc492b9f56f6e810505f6
Maxim Maslin <zapzarap@yandex.ru>
2020-01-27 13:30:21 +0300

    add redirect

--- docs/_redirects.js
+++ docs/_redirects.js
@@ -401,6 +401,14 @@ module.exports = [
     from: '/en/blockchain/waves-protocol/smart-contracts.html',
   },
   {
+    to: '/en/smart-contracts/what-is-smart-account',
+    from: '/en/blockchain/waves-protocol/smart-accounts',
+  },
+  {
+    to: '/en/smart-contracts/what-is-smart-account',
+    to: '/en/smart-contracts/smart-accounts',
+  },
+  {
     to: '/en/smart-contracts/smart-assets',
     from: '/en/technical-details/smart-assets.html',
   },
