3e5995bdf65ef128744c65588c099c568a5d4dd0
Maxim Maslin <zapzarap@yandex.ru>
2020-01-28 13:34:57 +0300

    add redirect on operators article

--- docs/_redirects.js
+++ docs/_redirects.js
@@ -610,6 +610,10 @@ module.exports = [
     to: '/en/ride',
   },
   {
+    from: '/en/ride/operators.html',
+    to: '/en/operators/operators',
+  },
+  {
     from: '/ru/ride/about-ride.html',
     to: '/ru/ride',
   },
